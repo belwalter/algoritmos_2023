347bc29086000edef099fefd6058ae711daca6d4
Bel Walter <belwalterv@gmail.com>
2023-03-13 20:09:27 -0300

    primeras instrucciones en python

---
+++ prueba.py
@@ -0,0 +1,38 @@
+
+#!TIPOS DE DATOS int float str bool None
+num = None
+
+num1 = 12
+num2 = 0.5
+# print(type(num2))
+# num2 = "Juan"
+# print(type(num2))
+# num2 = False
+# print(type(num2))
+
+#! OPERADORES ARITMETICOS + - * / // % **  ASIGNACION =
+
+NUM = 0
+num1 = 5
+num2 = 2
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num2 ** num1)
+
+#! FUNCION CCONVERSION int() str() floa() bool()
+numero_1 = int(input('Ingrese un numero: '))
+numero_2 = int(input('Ingrese otro numero: '))
+print(numero_1 + numero_2)
+
+#! CONDICIONAL
+
+#! CICLOS
+
+#! FUNCIONES
+
+#! ESTRUCTURAS DE DATOS
